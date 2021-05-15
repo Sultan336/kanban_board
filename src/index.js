ee41018dd0d0a16f17730604ceec31d2b26b415a
Sultan <m.sultan338@gmail.com>
2021-05-16 03:15:59 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => "Sup?";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
